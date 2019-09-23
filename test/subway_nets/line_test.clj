f07050b2b759d543569b2eaf65aa0441b58d1fee
David Omar Flores Chávez <david@davidomar.com>
2019-09-23 15:16:10 -0500

    Change number of tests to 100

--- test/subway_nets/line_test.clj
+++ test/subway_nets/line_test.clj
@@ -4,28 +4,32 @@
             [subway-nets.spec-check :as sc]
             [subway-nets.line :refer :all]))
 
+(def hundred-tests {:clojure.spec.test.check/opts {:num-tests 100}})
+
 (deftest subway-line-test
   "Check subway-line"
-  (is (sc/spec-check `subway-line {})))
+  (is (sc/spec-check `subway-line hundred-tests)))
 
 (deftest add-station-middle-test
   "Check add-station-middle"
-  (is (sc/spec-check `add-station-middle {})))
+  (is (sc/spec-check `add-station-middle hundred-tests)))
 
 (deftest add-station-end-test
   "Check add-station-end"
-  (is (sc/spec-check `add-station-end {})))
+  (is (sc/spec-check `add-station-end hundred-tests)))
 
 (deftest add-station-start-test
   "Check add-station-start"
-  (is (sc/spec-check `add-station-start {})))
+  (is (sc/spec-check `add-station-start hundred-tests)))
 
 (deftest extend-terminals-test
   "Check extend-terminals"
-  (is (sc/spec-check `extend-terminals {})))
+  (is (sc/spec-check `extend-terminals hundred-tests)))
 
-(t/check `subway-line)
-(t/check `add-station-middle)
-(t/check `add-station-end)
-(t/check `add-station-start)
-(t/check `extend-terminals)
+; ;; Uncomment following section when debugging. Makes it easier to track bugs.
+
+; (t/check `subway-line hundred-tests)
+; (t/check `add-station-middle hundred-tests)
+; (t/check `add-station-end hundred-tests)
+; (t/check `add-station-start hundred-tests)
+; (t/check `extend-terminals hundred-tests)
