020209ae4c6dfbbafb0005ca59ea48ebab6b4564
David Omar Flores Chávez <david@davidomar.com>
2019-09-23 14:33:14 -0500

    Add function specs for every function in <line.clj>
    Add a test-suite for line.clj.
    
    Define <::subway-line> which returns a generator of subway-lines for
    testing using <gen-subway-line>.

--- src/subway_nets/line.clj
+++ src/subway_nets/line.clj
@@ -2,6 +2,7 @@
   "Provides the functions to operate over or with subway lines"
   (:require [subway-nets.math :as math]
             [subway-nets.station :as sn-s]
+            [clojure.test.check.generators :as gen]
             [clojure.spec.alpha :as s]))
 
 ; ------------------------------- Create a line ------------------------------ ;
@@ -20,28 +21,98 @@
              #(= (-> % :ret :start) (-> % :ret :stations first))
              #(= (-> % :ret :end) (-> % :ret :stations last))))
 
+(def gen-subway-line
+  "Generator for subway lines"
+  (gen/fmap #(subway-line (first %) (second %))
+            (gen/tuple gen/string-alphanumeric (gen/vector gen/string-alphanumeric))))
+
+(s/def ::subway-line
+  (s/with-gen
+    (s/and 
+     #(>= (-> % :stations count) 2))
+    (constantly gen-subway-line)))
+
 ; --------------------------- Add stations to lines -------------------------- ;
 
 (defn add-station-middle
   "Takes a subway line and inserts the new station in the (floored) middle spot"
-  [line station]
-  (if (>= (count (:stations line)) 2)
-    (let [[before after]
-          (split-at (int (/ (count (:stations line)) 2))
-                    (:stations line))]
-      (assoc line :stations (vec (concat before [station] after))))))
-      
+  [line station]  
+  (let [[before after]
+        (split-at (int (/ (count (:stations line)) 2))
+                  (:stations line))]
+    (assoc line :stations (vec (concat before [station] after)))))
+
+(s/fdef add-station-middle
+  :args (s/cat
+         :line ::subway-line
+         :id string?)
+  :ret ::subway-line
+  :fn (s/and
+       ;; id of line and ret are equal
+       #(= (-> % :args :line :id)
+           (-> % :ret :id))
+       ;; size of ret is size of line + 1
+       #(= (-> % :args :line :stations count inc)
+           (-> % :ret :stations count))
+       ;; line and ret share the same start station
+       #(= (-> % :args :line :start)
+           (-> % :ret :start))
+       ;; line and ret share the same end station
+       #(= (-> % :args :line :end)
+           (-> % :ret :end))
+       ;; stations of ret contain the id of station
+       #(some #{(-> % :args :id)}
+              (-> % :ret :stations))
+       ))
+
 (defn add-station-end
   "Takes a line map and inserts a new station at the end of :stations line"
   [line station]
   (assoc (assoc line :stations (into (:stations line) [station])) :end station))
 
-(add-station-end (subway-line "" []) "x")
+(s/fdef add-station-end
+  :args (s/cat
+         :line ::subway-line
+         :id string?)
+  :ret ::subway-line
+  :fn (s/and
+       ;; id of line and ret are equal
+       #(= (-> % :args :line :id)
+           (-> % :ret :id))
+       ;; size of ret is size of line + 1
+       #(= (-> % :args :line :stations count inc)
+           (-> % :ret :stations count))
+       ;; line and ret share the same start station
+       #(= (-> % :args :line :start)
+           (-> % :ret :start))
+       ;; station and ret :end are the same station
+       #(= (-> % :args :station)
+           (-> % :ret :end :id))))
+
 
 (defn add-station-start
   "Takes a line map and inserts a new station at the start of :stations line"
   [line station]
   (assoc (assoc line :stations (into [station] (:stations line))) :start station))
+
+(s/fdef add-station-start
+  :args (s/cat
+         :line ::subway-line
+         :station string?)
+  :ret ::subway-line
+  :fn (s/and
+       ;; id of line and ret are equal
+       #(= (-> % :args :line :id)
+           (-> % :ret :id))
+       ;; size of ret is size of line + 1
+       #(= (-> % :args :line :stations count inc)
+           (-> % :ret :stations count))
+       ;; id and ret :end are the same station
+       #(= (-> % :args :line :end)
+           (-> % :ret :end))
+        ;; station and ret :start are the same station
+       #(= (-> % :args :station)
+           (-> % :ret :start))))
 
 
 ; ------------------------------- Extend a line ------------------------------ ;
@@ -59,3 +130,8 @@
          (if (= (inc size) limit)
            (add-station-start line "y")
            (recur (add-station-end (add-station-start line "y") "x") limit)))))))
+
+(s/fdef extend-terminals
+  :args (s/cat 
+         :line ::subway-line)
+  :ret ::subway-line)
--- test/subway_nets/line_test.clj
+++ test/subway_nets/line_test.clj
@@ -1,8 +1,31 @@
 (ns subway-nets.line-test
   (:require [clojure.test :refer :all]
+            [clojure.spec.test.alpha :as t]
             [subway-nets.spec-check :as sc]
             [subway-nets.line :refer :all]))
 
 (deftest subway-line-test
   "Check subway-line"
   (is (sc/spec-check `subway-line {})))
+
+(deftest add-station-middle-test
+  "Check add-station-middle"
+  (is (sc/spec-check `add-station-middle {})))
+
+(deftest add-station-end-test
+  "Check add-station-end"
+  (is (sc/spec-check `add-station-end {})))
+
+(deftest add-station-start-test
+  "Check add-station-start"
+  (is (sc/spec-check `add-station-start {})))
+
+(deftest extend-terminals-test
+  "Check extend-terminals"
+  (is (sc/spec-check `extend-terminals {})))
+
+(t/check `subway-line)
+(t/check `add-station-middle)
+(t/check `add-station-end)
+(t/check `add-station-start)
+(t/check `extend-terminals)
